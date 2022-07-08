41b94a228bbe61b5834e775cd2889b0a81a6cb33
Adam Leif <adamleif2024@u.northwestern.edu>
2022-07-08 03:40:41 -0500

    create outfiles when running

--- scripts/initial_scrape_test.sh
+++ scripts/initial_scrape_test.sh
@@ -1,3 +1,3 @@
 /projects/p31502/projects/craigslist/scripts/craig_env_runner 
 
-python -c 'import sys; sys.path.append("/projects/p31502/projects/craigslist/"); from cronable_scraping import *; do_initial_scrape("atlanta", filepath="../html")'
+python -c 'import sys; sys.path.append("/projects/p31502/projects/craigslist/"); from cronable_scraping import *; do_initial_scrape("atlanta", filepath="../html")' > /projects/p31502/projects/craigslist/scripts/outfiles/atlanta_out.out &
