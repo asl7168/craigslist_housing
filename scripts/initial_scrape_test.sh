e52b50a2da726bee4ee5b9dbe15834f583be0191
Adam Leif <adamleif2024@u.northwestern.edu>
2022-07-08 03:59:45 -0500

    setup initial_scrape for slurm queueing

--- scripts/initial_scrape_test.sh
+++ scripts/initial_scrape_test.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+#SBATCH -A p31502               # Allocation
+#SBATCH -p normal               # Queue
+#SBATCH -t 20:00:00             # Walltime/duration of the job
+#SBATCH -N 1                    # Number of Nodes
+#SBATCH --mem=18G               # Memory per node in GB needed for a job. Also see --mem-per-cpu
+#SBATCH --ntasks-per-node=6     # Number of Cores (Processors)
+#SBATCH --job-name="initial scrape test test"       # Name of job
+
 /projects/p31502/projects/craigslist/scripts/craig_env_runner 
 
 python -c 'import sys; sys.path.append("/projects/p31502/projects/craigslist/"); from cronable_scraping import *; do_initial_scrape("atlanta", filepath="../html")' > /projects/p31502/projects/craigslist/scripts/outfiles/atlanta_out.out &
