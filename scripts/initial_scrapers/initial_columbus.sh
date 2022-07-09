5022c41394668d9085854fc81aacc4c9293f3d60
Adam Leif <adamleif2024@u.northwestern.edu>
2022-07-09 00:31:26 -0500

    created 56 independent initial scrapers

---
+++ scripts/initial_scrapers/initial_columbus.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -A p31502               # Allocation
+#SBATCH -p normal               # Queue
+#SBATCH -t 20:00:00             # Walltime/duration of the job
+#SBATCH -N 1                    # Number of Nodes
+#SBATCH --mem=1G                # Memory per node in GB needed for a job. Also see --mem-per-cpu
+#SBATCH --ntasks-per-node=1     # Number of Cores (Processors)
+#SBATCH --output=../outfiles/columbus/columbus.out    # Path for output must already exist
+#SBATCH --error=../outfiles/columbus/columbus.err     # Path for errors must already exist
+#SBATCH --job-name="initial columbus scrape"       # Name of job
+
+/projects/p31502/projects/craigslist/scripts/craig_env_runner
+
+python -c 'import sys; sys.path.append("/projects/p31502/projects/craigslist/"); from cronable_scraping import *; do_initial_scrape("columbus", filepath="../../html")'
