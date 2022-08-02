8d8f5a19ca41b2a906fb8647d0596fc9c20d3258
Adam Leif <adamleif2024@u.northwestern.edu>
2022-08-01 19:07:46 -0500

    changes to cronable_scraping (don't fix current '0 posts, 0 duplicates' issue -- working on solution)

--- cronable_scraping.py
+++ cronable_scraping.py
@@ -36,50 +36,12 @@ class CraigslistScraper:
         self.base_url = f"https://{city}.craigslist.org/d/apartments-housing-for-rent/search/apa?availabilityMode=0&s="
         self.today_base_url = f"https://{city}.craigslist.org/d/apartments-housing-for-rent/search/apa?availabilityMode=0&postedToday=1&s="
 
-<<<<<<< HEAD
-        if proxy: self.proxy = {"http": f"http://{user}:{password}@{proxy}", "https": f"http://{user}:{password}@{proxy}"}
-
-    def check_url_status(self, url):
-        """ Checks a url for errors; if there aren't any, returns the Request object of the url; otherwise, 
-            outputs and logs the error
-
-        Parameters
-        ----------
-            url (string)
-
-        Returns
-        ----------
-            Request: the Request object of the parameter url
-        """
-        
-        print(url)
-
-        get_object = get(url, proxies=self.proxy)
-        if get_object.ok:
-            return get_object
-        else:
-            # create an error message
-            error_status = "Error: " + url
-            # get the time and date, for more info about the error/timed out
-            thisdate = date.today()
-            timestamp = time.time()
-            # create a file title (including save path)
-            filetitle = self.filepath + "error_log" + str(thisdate) + str(timestamp)
-            # create a block of text to put in an error log file
-            filetext = error_status + " | " + str(get_object)
-            # prints out the error and time info to the screen
-            print(error_status + ' at ' + str(thisdate) + str(timestamp))
-            # saves a file with error information to the current directory
-            with open(filetitle, 'w') as file:
-                file.write(filetext)
-=======
         if proxies: 
             self.curr_proxy = dict()
             self.avail_proxies = proxies
             self.unavail_proxies = []
         else: 
             self.curr_proxy = False
->>>>>>> 8469b6ec4da47573047110c5ec137d58e16c4d00
 
 
     def get_page_of_posts(self, url):
@@ -93,29 +55,35 @@ class CraigslistScraper:
         Returns
         ----------
             tuple: 
-                dict: {result id: [result url, result title]}
+                dict: {result id: result url}
                 bool: if scraping is finished (all duplicates or no remaining posts to check)
         """
         
-        print("\n\nGetting a search result page...")
+        print(f"\n\nGetting a search result page at url '{url}'...")
 
-        page_data = get(url, proxies=self.curr_proxy)
+        try:
+            page_data = get(url, proxies=self.curr_proxy)
+        except:
+            time.sleep(10)
+            page_data = get(url, proxies=self.curr_proxy)
+            
         html_soup = BeautifulSoup(page_data.text, 'html.parser')
         posts = html_soup.find_all('li', class_= 'result-row')
-        posts = posts if len(posts) <= 120 else posts[:int(html_soup.find(class_="rangeTo").getText())]
 
         posts_dict = {}
-        for one_post in posts:
-            one_post_title = one_post.find('a', class_='result-title hdrlnk')
-            post_id = one_post_title['id']
-            url = one_post_title['href']
-            posts_dict[post_id] = [url, one_post_title]
+        for post in posts:
+            post_title = post.find('a', class_='result-title hdrlnk')
+            post_id = post_title['id']
+            url = post_title['href']
+            posts_dict[post_id] = url
         
         init_len = len(posts_dict)
 
         for post_id in self.list_of_ids:
             if post_id in posts_dict.keys():
                 del posts_dict[post_id]
+            else:
+                self.list_of_ids.add(post_id)
 
         updated_len = len(posts_dict)
         print(f"Started with {init_len} posts, {init_len - updated_len} duplicates removed")
@@ -144,11 +112,15 @@ class CraigslistScraper:
             self.unavail_proxies.append(proxy_holder)
             
             post_id = key
-            values = dictionary_of_posts[key]
-            url = values[0]
+            url = dictionary_of_posts[key]
             filename = os.path.join(self.filepath, post_id)
             
-            raw_html = get(url, proxies=self.curr_proxy)
+            try:
+                raw_html = get(url, proxies=self.curr_proxy)
+            except:
+                time.sleep(10)
+                raw_html = get(url, proxies=self.curr_proxy)
+            
             with open(filename, 'w', encoding = 'utf-8') as file:
                 file.write(raw_html.text)
 
@@ -162,7 +134,7 @@ class CraigslistScraper:
             self.save_html_from_page(current_page_dict)
 
 
-    def get_posts_from_today(self):
+    def get_posts_from_today(self):  # TODO: update the logic of how this works; don't need the extra get in the while, clean this whole block up
         current_page = 0
         page_url = self.today_base_url
         total_pages = ceil(float(BeautifulSoup(get(page_url, proxies=self.curr_proxy).text, 'html.parser').find("span", class_="totalcount").text) / 120)
