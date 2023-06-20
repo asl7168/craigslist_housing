ce41b3612ea65a746c873bd19d5ec53d0cd8019d
asl7168 <adamleif2024@u.northwestern.edu>
2023-06-20 16:03:34 -0500

    train/dev/test now; use random seed; use same train/dev/test across multiple tasks

--- models/gpt3/gpt3_data_handling.py
+++ models/gpt3/gpt3_data_handling.py
@@ -7,7 +7,6 @@ from math import ceil
 import jsonlines 
 import openai
 from openai_credentials import skey, org 
-import tiktoken
 from tqdm import tqdm 
 
 openai.api_key = skey
@@ -61,36 +60,38 @@ def json_setup(city: str, only_body: bool = True):
       A stop sequence could be \n, ###, or any other token that does not appear in any completion.
     """
     cprint(f"Started json setup for {city}", c="y")
-
-    df = pd.read_csv(f"../../LLM_data/{city}_clean.csv")
-    
     prompt_fields = ["posting_body"] if only_body else ["posting_body", "title"]
 
+    df = pd.read_csv(f"../../LLM_data/{city}_clean.csv")
     df["posting_body"] = df["posting_body"].apply(lambda x: literal_eval(x))
-    df.dropna()  # shouldn't be any, but just in case
     
     def concat_body(body):
         return " ".join(body)  # turn list of strings into one string; TODO: fix this in text_processing
     
     df["posting_body"] = df["posting_body"].apply(concat_body)
     
-    # TODO: don't actually do body token checking here; do it elsewhere
-    encoding = tiktoken.get_encoding("r50k_base")  # remove prompts > 2048 tokens
-
-    def tokenize_body(body):
-        return len(encoding.encode(body))
-
-    df["body_tokens"] = df["posting_body"].apply(tokenize_body)
     df = df.astype({"title": "string", "posting_body": "string", "rent_class": "string", 
-                    "income_class": "string", "race": "string", "body_tokens": "int"})
-    df = df[df["body_tokens"] <= 2048]
+                    "income_class": "string", "race": "string"})
+    
+    test_df = df.sample(frac=0.2, random_state=1)
+    train_dev_df = df.drop(test_df.index)
+    train_df = train_dev_df.sample(frac=0.875, random_state=1)
+    dev_df = train_dev_df.drop(train_df.index)
+    
+    split_dir = "./split_csvs"
+    if not path.exists(split_dir): mkdir(split_dir)
 
+    train_df.to_csv(f"{split_dir}/{city}_train.csv")
+    dev_df.to_csv(f"{split_dir}/{city}_dev.csv")
+    test_df.to_csv(f"{split_dir}/{city}_test.csv")
+    
     for task in tasks_and_cols:
         cprint(f"    Making files for {task}", c="c")
         task_dir = f"./{task}"
         city_dir = f"{task_dir}/{city}"
         json_dir = f"{city_dir}/json_files"
-        filenames = [f"{json_dir}/{city}_{task}_train", f"{json_dir}/{city}_{task}_test"]
+        filenames = {"train": f"{json_dir}/{city}_{task}_train", "dev": f"{json_dir}/{city}_{task}_dev", 
+                     "test": f"{json_dir}/{city}_{task}_test"}
 
         if not path.exists(task_dir): mkdir(task_dir)
         if not path.exists(city_dir): mkdir(city_dir)
@@ -100,33 +101,32 @@ def json_setup(city: str, only_body: bool = True):
         
         for prompt_field in prompt_fields:
             cprint(f"        {prompt_field}", c="b")
-            filenames = [f"{f}_TITLE" for f in filenames] if prompt_field == "title" else filenames 
-            output_df = df[[prompt_field, col]]
-            output_df = output_df.rename(columns={prompt_field: "prompt", col: "completion"})
+            filenames = {k:f"{v}_TITLE" for (k, v) in filenames.items()} if prompt_field == "title" else filenames 
             
-            if task == "rent":
-                output_df["prompt"] = output_df["prompt"] + " Is rent cheap, average, or expensive?"
-            elif task == "income":
-                output_df["prompt"] = output_df["prompt"] + " Is income low, average, or high?"
-            elif task == "race":
-                output_df["prompt"] = output_df["prompt"] + " Is this area White or Non-White?"
+            output_dfs = {"train": train_df, "dev": dev_df, "test": test_df}
 
-            output_df["prompt"] = output_df["prompt"] + sep_tk
-            output_df["completion"] = " " + output_df["completion"] + " <STOP>"
-        
-            train_df = output_df.sample(frac=0.8)
-            train = train_df.to_dict(orient="records")
-            
-            test_df = output_df.drop(train_df.index)
-            test = test_df.to_dict(orient="records")
-            
-            for filename in filenames:
-                write_json_file(filename, train if "train" in filename else test)
+            for k in output_dfs:
+                output_df = output_dfs[k]
+                output_df = output_df[[prompt_field, col]]
+                output_df = output_df.rename(columns={prompt_field: "prompt", col: "completion"})
+
+                if task == "rent":
+                    output_df["prompt"] = output_df["prompt"] + " Is rent cheap, average, or expensive?"
+                elif task == "income":
+                    output_df["prompt"] = output_df["prompt"] + " Is income low, average, or high?"
+                elif task == "race":
+                    output_df["prompt"] = output_df["prompt"] + " Is this area White or Non-White?"
+
+                output_df["prompt"] = output_df["prompt"] + sep_tk
+                output_df["completion"] = " " + output_df["completion"] + " <STOP>"
+                
+                output_dict = output_df.to_dict(orient="records")
+                write_json_file(filenames[k], output_dict)
             
             cprint(f"        done", c="m")
 
         cprint(f"    done", c="m")
-
+    
     cprint(f"Completed json setup for {city}\n", c="g")
 
 
@@ -190,22 +190,22 @@ def make_completions(output_dir: str, model: str, n: int, df: pd.DataFrame, rand
     return df
 
 
-def completion_prep(city: str, task: str, n: int, randomize: bool):
+def completion_prep(city: str, task: str, n: int, randomize: bool, final_model: bool):
     prefix = f"./{task}/{city}"
     json_dir = f"{prefix}/json_files"
-    test_file = f"{json_dir}/{city}_{task}_test.jsonl"
+    t_file = f"{json_dir}/{city}_{task}_{'dev' if not final_model else 'test'}.jsonl"
     
     completions_dir = f"{prefix}/completions"
     if not path.exists(completions_dir): mkdir(completions_dir)
     
     df = None 
-    with jsonlines.open(test_file) as f:
+    with jsonlines.open(t_file) as f:
         df = pd.DataFrame(f)
 
     df = df.astype(str)
     df = df.rename(columns={"prompt": "prompt", "completion": "expected_completion"})
-
-    if randomize: df = df.sample(frac=1)  # shuffle df, keeping old indices (for now; probably won't matter)
+    
+    if randomize: df = df.sample(frac=1, random_state=1)  # shuffle df
 
     results_df = df.head(n)
     results_df["expected_completion"] = results_df["expected_completion"].str.slice(0,-7)  # remove " <STOP>" from end
@@ -214,35 +214,37 @@ def completion_prep(city: str, task: str, n: int, randomize: bool):
     return completions_dir, results_df
 
 
-def model_completions(city: str, task: str, model: list, n: int = 10, randomize: bool = True):
-    completions_dir, results_df = completion_prep(city, task, n, randomize)
+def model_completions(city: str, task: str, model: list, n: int = 10, randomize: bool = True, final_model: bool = False):
+    completions_dir, results_df = completion_prep(city, task, n, randomize, final_model)
 
     make_completions(completions_dir, model, n, results_df, randomize)
 
 
-def multimodel_completions(city: str, task: str, models: list, n: int = 10, randomize: bool = True):
-    completions_dir, results_df = completion_prep(city, task, n, randomize)
+def multimodel_completions(city: str, task: str, models: list, n: int = 10, randomize: bool = True, final_model: bool = False):
+    completions_dir, results_df = completion_prep(city, task, n, randomize, final_model)
 
     for m in models:
         make_completions(completions_dir, m, n, results_df, randomize)
 
 
-# TODO: some gpt4 completion maker. Ask Denis what our plan is with that and figure out how to get it going, etc.
-
 if __name__ == "__main__":
-    # ada_sizes = {5, 50, 500, 5000}
-    # json_setup("chicago")
-    # json_setup("seattle", only_body=False)
-    # write_train_subfiles("seattle", "rent", ada_sizes)
+    ada_sizes = {5, 50, 500}
+    json_setup("chicago")
+    write_train_subfiles("chicago", "rent", ada_sizes)
+
+    json_setup("seattle", only_body=False)
     
     # upload_train_files("chicago")
     # upload_train_files("seattle")
 
-    datasize_models = ["ada:ft-lingmechlab:seattle-rent-5-2023-06-19-23-33-36",
-                       "ada:ft-lingmechlab:seattle-rent-50-2023-06-19-23-37-04",
-                       "ada:ft-lingmechlab:seattle-rent-500-2023-06-19-23-46-08",
-                       "ada:ft-lingmechlab:seattle-rent-5000-2023-06-20-00-06-15",
-                       "ada:ft-lingmechlab:seattle-rent-2023-06-20-01-56-42"]
+    """old_datasize_models = ["ada:ft-lingmechlab:seattle-rent-5-2023-06-19-23-33-36",
+                           "ada:ft-lingmechlab:seattle-rent-50-2023-06-19-23-37-04",
+                           "ada:ft-lingmechlab:seattle-rent-500-2023-06-19-23-46-08",
+                           "ada:ft-lingmechlab:seattle-rent-5000-2023-06-20-00-06-15",
+                           "ada:ft-lingmechlab:seattle-rent-2023-06-20-01-56-42"]"""
+    
+    datasize_models = ["CHICAGO DATASIZE MODELS HERE"]
+    # TODO: MAKE SURE UPDATED COMPLETIONS CODE FOR DEV/TEST DIFFERENTIATION WORKS
     # multimodel_completions("seattle", "rent", datasize_models, n=100)
-    model_completions("seattle", "rent", "ada:ft-lingmechlab:seattle-rent-2023-06-20-01-56-42", 11984)
+    # model_completions("seattle", "rent", "ada:ft-lingmechlab:seattle-rent-2023-06-20-01-56-42", 11984)
     # cprint("Nothing to do right now!", c="m")
